765aff8ef053953d1ef5f1609265990cabfe4e7d
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2018-03-01 14:55:55 -0500

    minor changes and fixes
    changed the script to copy installation script to the nodes
    
    minor comment change on the instalation node

--- cluster/kubernetes/copy_dk_kub_install_script_to_nodes.sh
+++ cluster/kubernetes/copy_dk_kub_install_script_to_nodes.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-cssh -a " echo ' ' > docker_kubernites_install.sh \
- && scp pi@rp0:~/docker_kubernites_install.sh docker_kubernites_install.sh \
- && sh docker_kubernites_install.sh"
+#copying the script to nodes
+for number in {1..4}
+	do 
+		scp /home/pi/docker_kubernites_install.sh \
+			pi@rp$number:/home/pi/docker_kubernites_install.sh
+	done
+exit 0
+
+#running the script in the nodes
+cssh -a "sh docker_kubernites_install.sh"
--- cluster/kubernetes/docker_kubernites_install.sh
+++ cluster/kubernetes/docker_kubernites_install.sh
@@ -11,8 +11,8 @@ sudo dphys-swapfile swapoff \
   && sudo update-rc.d dphys-swapfile -qy remove  
 echo Adding " cgroup_enable=cpuset cgroup_enable=memory" to /boot/cmdline.txt
 sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
-# if you encounter problems, try changing cgroup_memory=1 to 
-#cgroup_enable=memory.
+# if you encounter problems, try changing cgroup_memory=memory to 
+#cgroup_enable=1.
 orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=memory"
 echo $orig | sudo tee /boot/cmdline.txt
 
