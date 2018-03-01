20dc7cdbb7e4c4dfa1ee46fbb24560d4dfc22519
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2018-03-01 12:14:10 -0500

    changed part of the script back

--- cluster/kubernetes/script_install_slitelly_modified.sh
+++ cluster/kubernetes/script_install_slitelly_modified.sh
@@ -16,10 +16,9 @@ sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
 orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1"
 echo $orig | sudo tee /boot/cmdline.txt
 
+
 # Add repo list and install kubeadm
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg \ 
-  | sudo apt-key add - \
-  && echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" \ 
-  | sudo tee /etc/apt/sources.list.d/kubernetes.list \
-  && sudo apt-get update -qy \
-  && sudo apt-get install -qy kubeadm
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
+  echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
+  sudo apt-get update -q && \
+  sudo apt-get install -qy kubeadm
