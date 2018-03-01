83384d019c6b4ef7a06a28fcfce19057f59a23bb
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2018-03-01 12:44:37 -0500

    changed cgroup memory parameter

--- cluster/kubernetes/script_install_slitelly_modified.sh
+++ cluster/kubernetes/script_install_slitelly_modified.sh
@@ -13,7 +13,7 @@ echo Adding " cgroup_enable=cpuset cgroup_enable=memory" to /boot/cmdline.txt
 sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
 # if you encounter problems, try changing cgroup_memory=1 to 
 #cgroup_enable=memory.
-orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1"
+orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=memory"
 echo $orig | sudo tee /boot/cmdline.txt
 
 
@@ -22,3 +22,5 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
   echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
   sudo apt-get update -q && \
   sudo apt-get install -qy kubeadm
+#reboot
+sudo reboot 
